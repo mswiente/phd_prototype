13ee0fbcafb9d30e2a299a821da588173cd1a9bb
Martin Swientek <martin@swientek.org>
2012-02-19 18:44:05 +0100

    example configuration

--- BatchProcessor/src/main/java/com/jswiente/phd/ExampleItemWriter.java
+++ BatchProcessor/src/main/java/com/jswiente/phd/ExampleItemWriter.java
@@ -20,6 +20,7 @@ public class ExampleItemWriter implements ItemWriter<Object> {
 	 * @see ItemWriter#write(Object)
 	 */
 	public void write(List<? extends Object> data) throws Exception {
+		log.info("data size: " + data.size());
 		log.info(data);
 	}
 
