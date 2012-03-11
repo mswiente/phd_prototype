56e6a274e0d6315af29f1155e3a1b72be0456385
Martin Swientek <martin@swientek.org>
2012-03-11 11:45:04 +0100

    MasterDataGenerator

--- DataGenerator/src/main/java/com/jswiente/phd/prototype/DataGenerator/MasterDataGenerator.java
+++ DataGenerator/src/main/java/com/jswiente/phd/prototype/DataGenerator/MasterDataGenerator.java
@@ -2,7 +2,11 @@ package com.jswiente.phd.prototype.DataGenerator;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.swing.text.DefaultEditorKit.CutAction;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +21,8 @@ import com.jswiente.phd.prototype.domain.*;
 import com.jswiente.phd.prototype.persistence.AccountDAO;
 import com.jswiente.phd.prototype.persistence.CustomerDAO;
 import com.jswiente.phd.prototype.persistence.CustomerproductDAO;
+import com.jswiente.phd.prototype.persistence.CustomerproducttariffDAO;
+import com.jswiente.phd.prototype.persistence.EventsourceDAO;
 import com.jswiente.phd.prototype.persistence.ProductDAO;
 import com.jswiente.phd.prototype.persistence.TariffDAO;
 import com.jswiente.phd.prototype.utils.DataUtils;
@@ -36,8 +42,16 @@ public class MasterDataGenerator {
 	private TariffDAO tariffDAO;
 	@Autowired
 	private CustomerproductDAO customerProductDAO;
+	@Autowired
+	private CustomerproducttariffDAO customerProductTariffDao;
+	@Autowired
+	private EventsourceDAO eventSourceDAO;
 	
-	private long customerProductId = 0;
+	private long customerProductId = 1;
+	private long customerProductTariffId = 1;
+	private long eventSourceId = 0;
+	
+	private Map<Long, Tariff> productTariffsMap = new HashMap<Long, Tariff>();   
 	
 	public MasterDataGenerator() {
 	}
@@ -55,11 +69,17 @@ public class MasterDataGenerator {
 		logger.debug("getting tariffs from db...");
 		List<Tariff> tariffs = tariffDAO.getAllTariffs();
 		logger.debug("tariffs fetched: " + tariffs.size());
+		
+		//building productsTariffsMap
+		int i = 0;
+		for (Product product : products) {
+			productTariffsMap.put(product.getProductId(), tariffs.get(i++));
+		}
 			
 		List<Account> accounts = generateAccounts(customers);
-		List<Customerproduct> customerProducts = generateCustomerProducts(accounts, products, tariffs);
-		
 		accountDAO.persistAll(accounts);
+		generateCustomerProducts(accounts, products, tariffs);
+		
 	}
 	
 	private List<Account> generateAccounts(List<Customer> customers) {
@@ -82,27 +102,51 @@ public class MasterDataGenerator {
 		return accounts;
 	}
 	
-	private List<Customerproduct> generateCustomerProducts(List<Account> accounts, List<Product> allProducts, List<Tariff> allTariffs) {
-		List<Customerproduct> customerProducts = new ArrayList<Customerproduct>();
+	private void generateCustomerProducts(List<Account> accounts, List<Product> allProducts, List<Tariff> allTariffs) {
+		Date startDate = DataUtils.getFirstDayOfMonth().toDate();
 		for (Account account : accounts) {
 			int productQty = DataUtils.getRandomInteger(10);
 			List<Product> products = DataUtils.getRandomElements(allProducts, productQty);
 			int productSeq = 1;
 			for (Product product : products) {
 				Customerproduct customerProduct = new Customerproduct();
-				customerProduct.setCustomerProductId(customerProductId);
+				customerProduct.setCustomerProductId(customerProductId++);
 				customerProduct.setAccountNum(account.getAccountNum());
 				customerProduct.setCustomer(account.getCustomer());
 				customerProduct.setProduct(product);
 				customerProduct.setProductSeq(productSeq++);
 				customerProduct.setEndDate(null);
-				customerProduct.setStartDate(DataUtils.getFirstDayOfMonth().toDate());
+				customerProduct.setStartDate(startDate);
 				
-				customerProducts.add(customerProduct);
+				Customerproducttariff customerProductTariff = new Customerproducttariff();
+				customerProductTariff.setCustomerProductTariffId(customerProductTariffId++);
+				customerProductTariff.setCustomerproduct(customerProduct);
+				customerProductTariff.setTariff(productTariffsMap.get(product.getProductId()));
+				customerProductTariff.setStartDate(startDate);
+				customerProductTariff.setEndDate(null);
+				
+				Eventsource eventSourceVoice = new Eventsource();
+				eventSourceVoice.setEventSourceId(eventSourceId++);
+				eventSourceVoice.setCustomerproduct(customerProduct);
+				eventSourceVoice.setEventType(EventType.VOICE);
+				eventSourceVoice.setEventSource(generateEventSource(account.getCustomer().getPhone(), productSeq));
+				
+				Eventsource eventSourceData = new Eventsource();
+				eventSourceData.setEventSourceId(eventSourceId++);
+				eventSourceData.setCustomerproduct(customerProduct);
+				eventSourceData.setEventType(EventType.DATA);
+				eventSourceData.setEventSource(generateEventSource(account.getCustomer().getPhone(), productSeq));
+				
+				customerProductDAO.persist(customerProduct);
+				customerProductTariffDao.persist(customerProductTariff);
+				eventSourceDAO.persist(eventSourceVoice);
+				eventSourceDAO.persist(eventSourceData);
 			}
 		}
-		
-		return customerProducts;
+	}
+	
+	private static String generateEventSource(String input, int productSeq) {
+		return "+49" + input.replaceAll("/", "").replaceAll(" ", "").substring(1) + productSeq;
 	}
 	
 	public static void main(String[] args) {
