f273d3f6940413784a2c08e2ac98530ee2ee0587
Martin Swientek <martin@swientek.org>
2012-03-20 22:13:03 +0100

    datamodel changes

---
+++ FunctionalCore/src/main/java/com/jswiente/phd/prototype/persistence/CalldetailDAO.java
@@ -0,0 +1,71 @@
+package com.jswiente.phd.prototype.persistence;
+
+// Generated 20.03.2012 22:03:30 by Hibernate Tools 3.4.0.CR1
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.jswiente.phd.prototype.domain.Calldetail;
+
+/**
+ * Home object for domain model class Calldetail.
+ * @see com.jswiente.phd.prototype.persistence.Calldetail
+ * @author Hibernate Tools
+ */
+@Stateless
+public class CalldetailDAO {
+
+	private static final Logger logger = LoggerFactory.getLogger(CalldetailDAO.class);
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public void persist(Calldetail transientInstance) {
+		logger.debug("persisting Calldetail instance");
+		try {
+			entityManager.persist(transientInstance);
+			logger.debug("persist successful");
+		} catch (RuntimeException re) {
+			logger.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void remove(Calldetail persistentInstance) {
+		logger.debug("removing Calldetail instance");
+		try {
+			entityManager.remove(persistentInstance);
+			logger.debug("remove successful");
+		} catch (RuntimeException re) {
+			logger.error("remove failed", re);
+			throw re;
+		}
+	}
+
+	public Calldetail merge(Calldetail detachedInstance) {
+		logger.debug("merging Calldetail instance");
+		try {
+			Calldetail result = entityManager.merge(detachedInstance);
+			logger.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			logger.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Calldetail findById(long id) {
+		logger.debug("getting Calldetail instance with id: " + id);
+		try {
+			Calldetail instance = entityManager.find(Calldetail.class, id);
+			logger.debug("get successful");
+			return instance;
+		} catch (RuntimeException re) {
+			logger.error("get failed", re);
+			throw re;
+		}
+	}
+}
---
+++ FunctionalCore/src/main/java/com/jswiente/phd/prototype/persistence/SkippedeventDAO.java
@@ -0,0 +1,71 @@
+package com.jswiente.phd.prototype.persistence;
+
+// Generated 20.03.2012 22:03:30 by Hibernate Tools 3.4.0.CR1
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.jswiente.phd.prototype.domain.Skippedevent;
+
+/**
+ * Home object for domain model class Skippedevent.
+ * @see com.jswiente.phd.prototype.persistence.Skippedevent
+ * @author Hibernate Tools
+ */
+@Stateless
+public class SkippedeventDAO {
+
+	private static final Logger logger = LoggerFactory.getLogger(SkippedeventDAO.class);
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public void persist(Skippedevent transientInstance) {
+		logger.debug("persisting Skippedevent instance");
+		try {
+			entityManager.persist(transientInstance);
+			logger.debug("persist successful");
+		} catch (RuntimeException re) {
+			logger.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void remove(Skippedevent persistentInstance) {
+		logger.debug("removing Skippedevent instance");
+		try {
+			entityManager.remove(persistentInstance);
+			logger.debug("remove successful");
+		} catch (RuntimeException re) {
+			logger.error("remove failed", re);
+			throw re;
+		}
+	}
+
+	public Skippedevent merge(Skippedevent detachedInstance) {
+		logger.debug("merging Skippedevent instance");
+		try {
+			Skippedevent result = entityManager.merge(detachedInstance);
+			logger.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			logger.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Skippedevent findById(long id) {
+		logger.debug("getting Skippedevent instance with id: " + id);
+		try {
+			Skippedevent instance = entityManager.find(Skippedevent.class, id);
+			logger.debug("get successful");
+			return instance;
+		} catch (RuntimeException re) {
+			logger.error("get failed", re);
+			throw re;
+		}
+	}
+}
