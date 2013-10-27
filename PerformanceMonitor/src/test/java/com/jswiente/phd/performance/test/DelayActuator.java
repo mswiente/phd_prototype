74cef5876ecea8f13b422169624f882c6d1d3830
Martin Swientek <martin@swientek.org>
2013-10-27 12:35:02 +0100

    umbenannt

---
+++ PerformanceMonitor/src/test/java/com/jswiente/phd/performance/test/DelayActuator.java
@@ -0,0 +1,45 @@
+package com.jswiente.phd.performance.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.jswiente.phd.performance.Actuator;
+import com.jswiente.phd.performance.DefaultNotificationListener;
+
+public class DelayActuator implements Actuator {
+	
+	private static final Logger logger = LoggerFactory
+	.getLogger(DefaultNotificationListener.class);
+	
+	@Autowired
+	private MonitoredService monitoredService;
+	@Value("${controller.interval}")
+	private int interval;
+
+	@Override
+	public void onLowThreshold() {
+		int oldValue = monitoredService.getDelay();
+		int newValue = oldValue - interval;
+		monitoredService.setDelay(newValue);
+		logger.debug("Setting delay: oldValue: " + oldValue + ", newValue: " + newValue);
+	}
+
+	@Override
+	public void onHighThreshold() {
+		int oldValue = monitoredService.getDelay();
+		int newValue = oldValue + interval;
+		monitoredService.setDelay(newValue);
+		logger.debug("Setting delay: oldValue: " + oldValue + ", newValue: " + newValue);
+	}
+
+	public void setMonitoredService(MonitoredService monitoredService) {
+		this.monitoredService = monitoredService;
+	}
+
+	public void setInterval(int interval) {
+		this.interval = interval;
+	}
+
+}
